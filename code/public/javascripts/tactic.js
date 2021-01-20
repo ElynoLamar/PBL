74cd7602d997a8a5ecbc014b0a246917bdb63f7b
Jaime Ferreira <jaimeferreira2000@gmail.com>
2021-01-20 15:17:34 +0000

    +design update finnished

---
+++ code/public/javascripts/tactic.js
@@ -0,0 +1,16 @@
+function show(index) {
+    switch (index) {
+        case 0:
+            window.location = "../index.html";
+            break;
+        case 1:
+            window.location = "team.html";
+            break;
+        case 2:
+            window.location = "event.html";
+            break;
+        case 3:
+            window.location = "map.html";
+            break;
+    }
+}
