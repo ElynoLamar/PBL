796641e0171c3ef7541348e456e1d2c14af16519
Elyno <fernandolamarsantos@gmail.com>
2021-02-04 09:27:22 +0000

    nova DB porque a outra estava congestionada e so permitia 1 acesso de cada vez
    nao conseguia aceder com o localhost + diretamente na DB

--- code/models/connection.js
+++ code/models/connection.js
@@ -1,32 +1,41 @@
 const util = require('util')
 const mysql = require('mysql')
 const pool = mysql.createPool({
-  connectionLimit: 50,
-  host: 'remotemysql.com',
-  port: 3306,
-  user: 'kST6V7n37P',
-  password: 'bONWToYUUj',
-  database: 'kST6V7n37P'
+    /**
+          connectionLimit: 300,
+          host: 'remotemysql.com',
+          port: 3306,
+          user: 'kST6V7n37P',
+          password: 'bONWToYUUj',
+          database: 'kST6V7n37P'
+    */
+    connectionLimit: 300,
+    host: 'remotemysql.com',
+    port: 3306,
+    user: 'eYxyFJmtSn',
+    password: '6aXdNVugKW',
+    database: 'eYxyFJmtSn'
+
 })
 
 
 // Ping database to check for common exception errors.
 pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.')
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
     }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.')
-    }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.')
-    }
-  }
 
-  if (connection) connection.release()
+    if (connection) connection.release()
 
-  return
+    return
 })
 
 // Promisify for Node.js async/await.
